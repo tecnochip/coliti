477863ce16f55117a343f323c497b2ca0d6ef64f
SergioMonteroVadillo <84685043+SergioMonteroVadillo@users.noreply.github.com>
2021-06-15 12:35:11 +0200

    Programa con datos por ventana
    Este código ya admite que se escriban los planetas en mayúsculas o minúsculas y tiene un try catch por si se introdujesen letras donde debe de haber números

--- TiroParabolico2/src/view/Controlador.java
+++ TiroParabolico2/src/view/Controlador.java
@@ -15,15 +15,19 @@ public class Controlador {
 
 		this.vista.Boton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				try {
+					Calculos datos = 
+							new Calculos(vista.getTextoPlaneta().getText(),
+									Double.parseDouble(vista.getTextoVelocidad().getText()),
+									Double.parseDouble(vista.getTextoAngulo().getText()),									 
+									Double.parseDouble(vista.getTextoSegundo().getText()));
 
-				Calculos datos = 
-						new Calculos(vista.getTextoPlaneta().getText(),
-								Double.parseDouble(vista.getTextoVelocidad().getText()),
-								Double.parseDouble(vista.getTextoAngulo().getText()),									 
-								Double.parseDouble(vista.getTextoSegundo().getText()));
+					vista.getTextoResultados().setText(datos.toString());
+					vista.getTextoEstudio().setText(datos.Imprimir());	
 
-				vista.getTextoResultados().setText(datos.toString());
-				vista.getTextoEstudio().setText(datos.Imprimir());					
+				}catch (java.lang.NumberFormatException ex) {
+					System.out.println ("Los datos o algun dato introducidos no es correcto");
+				}
 			}			
 		});		
 	}
